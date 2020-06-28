5f45bd7fe5e7cc038b9fccc69c61e1afcf7aff7d
Brian J. Cardiff <bcardiff@gmail.com>
2020-06-28 00:33:04 -0300

    Expose all session API

--- src/fswatch.cr
+++ src/fswatch.cr
@@ -30,8 +30,34 @@ module FSWatch
     end
   end
 
+  alias EventFlag = LibFSWatch::EventFlag
+
+  record EventTypeFilter, flag : EventFlag
+
+  alias FilterType = LibFSWatch::FilterType
+
+  record MonitorFilter, text : String, type : FilterType, case_sensitive : Bool, extended : Bool
+
   def self.init
-    check LibFSWatch.init_library, "Unable to init fswatch"
+    check LibFSWatch.init_library, "Unable to init_library"
+  end
+
+  def self.verbose
+    LibFSWatch.is_verbose != 0
+  end
+
+  def self.verbose=(value : Bool)
+    LibFSWatch.set_verbose(value)
+    value
+  end
+
+  def self.event_flag_by_name(name : String) : EventFlag
+    check LibFSWatch.get_event_flag_by_name(name, out flag), "Unable to event_flag_by_name"
+    flag
+  end
+
+  def self.event_flag_name(flag : EventFlag)
+    String.new(LibFSWatch.get_event_flag_name(flag))
   end
 
   private def self.check(status, message)
@@ -97,13 +123,75 @@ module FSWatch
       check LibFSWatch.is_running(@handle), "Unable to is_running"
     end
 
+    def latency=(value : Float64)
+      check LibFSWatch.set_latency(@handle, value), "Unable to set_latency"
+    end
+
+    def recursive=(value : Bool)
+      check LibFSWatch.set_recursive(@handle, value), "Unable to set_recursive"
+    end
+
+    def directory_only=(value : Bool)
+      check LibFSWatch.set_directory_only(@handle, value), "Unable to set_directory_only"
+    end
+
+    def follow_symlinks=(value : Bool)
+      check LibFSWatch.set_follow_symlinks(@handle, value), "Unable to set_follow_symlinks"
+    end
+
+    def add_property(name : String, value : String)
+      check LibFSWatch.add_property(@handle, name, value), "Unable to add_property"
+    end
+
+    def allow_overflow=(value : Bool)
+      check LibFSWatch.set_allow_overflow(@handle, value), "Unable to set_allow_overflow"
+    end
+
+    def add_event_type_filter(event_type : EventTypeFilter)
+      etv = LibFSWatch::EventTypeFilter.new
+      etv.flag = event_type.flag
+      check LibFSWatch.add_event_type_filter(@handle, etv), "Unable to add_event_type_filter"
+    end
+
+    def add_filter(monitor_filter : MonitorFilter)
+      cmf = LibFSWatch::CmonitorFilter.new
+      cmf.text = monitor_filter.text.to_unsafe
+      cmf.type = monitor_filter.type
+      cmf.case_sensitive = monitor_filter.case_sensitive
+      cmf.extended = monitor_filter.extended
+      check LibFSWatch.add_filter(@handle, cmf), "Unable to add_filter"
+    end
+
     private def check(status, message)
       raise Error.new(message) unless status == LibFSWatch::OK
     end
   end
 
-  def self.watch(path : String | Path, &block : Event ->)
+  def self.watch(path : String | Path, *,
+                 latency : Float64? = nil,
+                 recursive : Bool? = nil,
+                 directory_only : Bool? = nil,
+                 follow_symlinks : Bool? = nil,
+                 allow_overflow : Bool? = nil,
+                 properties : Hash(String, String)? = nil,
+                 event_type_filters : Array(EventTypeFilter)? = nil,
+                 filters : Array(MonitorFilter)? = nil,
+                 &block : Event ->)
     session = FSWatch::Session.new
+    session.latency = latency unless latency.nil?
+    session.recursive = recursive unless recursive.nil?
+    session.directory_only = directory_only unless directory_only.nil?
+    session.follow_symlinks = follow_symlinks unless follow_symlinks.nil?
+    session.allow_overflow = allow_overflow unless allow_overflow.nil?
+    if properties
+      properties.each { |k, v| session.add_property(k, v) }
+    end
+    if event_type_filters
+      event_type_filters.each { |etv| session.add_event_type_filter(etv) }
+    end
+    if filters
+      filters.each { |f| session.filters(etv) }
+    end
     session.on_change(&block)
     session.add_path path
     session.start_monitor
